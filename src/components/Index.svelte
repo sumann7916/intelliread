282aa42da310d57e780c31c1cf5b425826a51ad8
sumann7916 <khadkasuman324@gmail.com>
2023-07-02 16:15:51 +0545

    forgot what i added

---
+++ src/components/Index.svelte
@@ -0,0 +1,20 @@
+<script>
+    import summarizeIcon from './summarize.png';
+    import searchIcon from './search.png';
+    
+	let name = ""
+	const handleSummarize = () => {
+	
+	}
+
+	
+</script>
+
+<div class="flex bg-gray-100">
+    <button on:click={handleSummarize} class="p-2 w-12 h-12 relative hover:bg-gray-200 focus:outline-none focus:bg-gray-200">
+        <img alt="summarizer-icon" src={summarizeIcon} class="w-6 h-6">
+    </button>
+    <button class="p-2 w-12 h-12 relative hover:bg-gray-200 focus:outline-none focus:bg-gray-200">
+        <img alt="search-icon" src={searchIcon} class="w-6 h-6">
+    </button>
+</div>
--- src/popup/index.ts
+++ src/popup/index.ts
@@ -1,1 +1,9 @@
 import "../app.css";
+import Index from "../components/Index.svelte";
+
+const target = document.getElementById("app");
+
+async function render() {
+  new Index({ target });
+}
+document.addEventListener("DOMContentLoaded", render);
